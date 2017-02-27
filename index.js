4a8dd5d3448f7170859f2c0b6a19905f49189216
Arjun Balaji <arjunblj@gmail.com>
2017-02-26 19:44:17 -0500

    Add basic result fetching w/ keywords.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+require('isomorphic-fetch')
+
+const config = require('./config.json')
+
+const WATCH_RECON_URL_BASE = 'http://www.watchrecon.com/?view=json&page_size=50&last_days=7&origin=ioslisting'
+
+function getWatchResults () {
+  config.keywords.forEach(async keyword => {
+    const response = await fetch(
+      `${WATCH_RECON_URL_BASE}&query=${encodeURIComponent(keyword)}`
+    )
+    const data = await response.json()
+    data.listings.map(listing => console.log(listing))
+  })
+}
+
+getWatchResults()
